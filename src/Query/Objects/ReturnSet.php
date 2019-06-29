4b79a32cc92d5f9a45fcc1b8273dc28cfc18c7e4
WhizSid <whizsid@aol.com>
2019-06-30 04:35:14 +0530

    getter for affected rows

--- src/Query/Objects/ReturnSet.php
+++ src/Query/Objects/ReturnSet.php
@@ -4,107 +4,6 @@ namespace WhizSid\ArrayBase\Query\Objects;
 
 use WhizSid\ArrayBase\AB\DataSet;
 
-<<<<<<< HEAD
-class ReturnSet {
-	/**
-	 * DataSet that query has returned
-	 *
-	 * @var DataSet
-	 */
-	protected $dataSet;
-	/**
-	 * Count of affected rows
-	 *
-	 * @var int
-	 */
-	protected $affectedRows=0;
-	/**
-	 * Count of rows that in data set
-	 *
-	 * @var int
-	 */
-	protected $count=0;
-	/**
-	 * Query execution time
-	 *
-	 * @var float
-	 */
-	protected $time = 0.00;
-	/**
-	 * Last Index for insert queries
-	 *
-	 * @var integer
-	 */
-	protected $lastIndex = 0;
-	/**
-	 * Setter for data set
-	 *
-	 * @param DataSet $dataSet
-	 * @return void
-	 */
-	public function setDataSet($dataSet){
-		$this->dataSet = $dataSet;
-		$this->count = $dataSet->getCount();
-	}
-	/**
-	 * Setter for affected row count
-	 * 
-	 * @param int $count
-	 * @return void
-	 */
-	public function setAffectedRowsCount($count){
-		$this->affectedRows = $count;
-	}
-	/**
-	 * Setter for time
-	 *
-	 * @param float $time
-	 * @return void
-	 */
-	public function setTime($time){
-		$this->time = $time;
-	}
-	/**
-	 * Returning the execution time in micro seconds
-	 *
-	 * @return float
-	 */
-	public function getTime(){
-		return $this->time;
-	}
-	/**
-	 * Setter for last index
-	 *
-	 * @param int $index
-	 * @return void
-	 */
-	public function setLastIndex($index){
-		$this->lastIndex = $index;
-	}
-	/**
-	 * Returning the last inserted index for insert queries
-	 *
-	 * @return integer
-	 */
-	public function getLastIndex(){
-		return $this->lastIndex;
-	}
-	/**
-	 * Returning the count of affected rows by the query
-	 *
-	 * @return int
-	 */
-	public function getAffectedRowsCount(){
-		return $this->affectedRows;
-	}
-	/**
-	 * Fetching the data set to associative array
-	 *
-	 * @return void
-	 */
-	public function fetchAssoc(){
-		$fetched = [];
-=======
 class ReturnSet
 {
     /**
@@ -151,6 +50,16 @@ class ReturnSet
         $this->count = $dataSet->getCount();
     }
 
+    /**
+     * Setter for data set.
+     *
+     * @return int
+     */
+    public function getAffectedRowsCount()
+    {
+        return $this->affectedRows;
+	}
+	
     /**
      * Setter for affected row count.
      *
@@ -215,7 +124,6 @@ class ReturnSet
     public function fetchAssoc()
     {
         $fetched = [];
->>>>>>> 6dc8438dcd5eabecaeb38c442615508b46b130ca
 
         if (!isset($this->dataSet)) {
             return [];
